60a2af5c8251c5c156b1678f83956b785f1c6880
TediKina <kinatedi@gmail.com>
2022-12-20 16:35:33 +0100

    update

--- project1/src/App.js
+++ project1/src/App.js
@@ -9,19 +9,23 @@ import {
 import "./App.css";
 import Users from "./user/pages/Users";
 import NewPlaces from "../src/places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlaces />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlaces />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
